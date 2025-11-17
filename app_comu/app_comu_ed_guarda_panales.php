57385cf851fb023150d78823e1a1295b526db720
Mario Fèvre <mario@trecc.com.ar>
2025-11-17 13:54:08 -0300

    Add files via upload

---
+++ app_comu/app_comu_ed_guarda_panales.php
@@ -0,0 +1,161 @@
+<?php
+/**
+* 
+ * 
+ ***  
+* @package    	geoTReCC
+* @author     	TReCC SA
+* @author     	<mario@trecc.com.ar>
+* @author    	http://www.trecc.com.ar
+* @author		based on TReCC SA Panel de control. https://github.com/mariofevre/TReCC---Panel-de-Control/
+* @copyright	2025 TReCC SA* 
+* @copyright	esta aplicación se desarrollo sobre una publicación GNU 2018 - Universidad de Buenos Aires - geoGEC
+* @copyright	esta aplicación se desarrollo sobre una publicación GNU 2017 TReCC SA
+* @license    	http://www.gnu.org/licenses/gpl.html GNU AFFERO GENERAL PUBLIC LICENSE, version 3 (GPL-3.0)
+* Este archivo es software libre: tu puedes redistriburlo 
+* y/o modificarlo bajo los términos de la "GNU AFFERO GENERAL PUBLIC LICENSE" 
+* publicada por la Free Software Foundation, version 3
+* 
+* Este archivo es distribuido por si mismo y dentro de sus proyectos 
+* con el objetivo de ser útil, eficiente, predecible y transparente
+* pero SIN NIGUNA GARANTÍA; sin siquiera la garantía implícita de
+* Consulte la "GNU General Public License" para más detalles.
+* 
+* Si usted no cuenta con una copia de dicha licencia puede encontrarla aquí: <http://www.gnu.org/licenses/>.
+* 
+*
+*/
+
+ini_set('display_errors', 1);
+chdir('..');
+include_once("./_config_acc/claveunica.php");
+include_once("./_config_inst/config.php");
+
+// verificación de seguridad 
+if(!isset($_SESSION)) {
+	session_start(); 
+	if(!isset($_SESSION[$CU]["usuario"]['id'])){
+		$_SESSION[$CU]["usuario"]['id']='-1';
+	}
+}
+
+include_once('./comun_general/encabezado.php');
+include_once("./comun_general/pgqonect.php");
+include_once("./sis_usuarios/usu_validacion.php");
+$Usu = validarUsuario(); // en ./usu_valudacion.php
+
+// funciones frecuentes
+include("./comun_general/fechas.php");
+include("./comun_general/cadenas.php");
+
+$COD = isset($_GET['cod'])?$_GET['cod'] : '';
+$ID = isset($_GET['id'])?$_GET['id'] : '';
+if($ID==''&&$COD==''){
+	header('location: ./index.php');
+}
+
+$Hoy_a = date("Y");$Hoy_m = date("m");$Hoy_d = date("d"); $HOY = $Hoy_a."-".$Hoy_m."-".$Hoy_d;	
+
+$Log['data']=array();
+$Log['tx']=array();
+$Log['mg']=array();
+$Log['res']='';
+
+function terminar($Log){
+	$res=json_encode($Log);
+	if($res==''){$res=print_r($Log,true);}
+	echo $res;
+	exit;
+}
+
+if(!isset($_POST['codMarco'])){
+	$Log['tx'][]='no fue enviada la variable codMarco';
+	$Log['res']='err';
+	terminar($Log);	
+}
+
+$minacc=0;
+if(isset($_POST['nivelPermiso'])){
+    $minacc=$_POST['nivelPermiso'];
+}
+
+$Acc=0;
+if(isset($Usu['acc']['est_02_marcoacademico'][$_POST['codMarco']]['app_rele'])){
+	$Acc=$Usu['acc']['est_02_marcoacademico'][$_POST['codMarco']]['app_rele'];
+}elseif(isset($Usu['acc']['est_02_marcoacademico'][$_POST['codMarco']]['general'])){
+	$Acc=$Usu['acc']['est_02_marcoacademico'][$_POST['codMarco']]['general'];
+}elseif(isset($Usu['acc']['est_02_marcoacademico']['general']['general'])){
+	$Acc=$Usu['acc']['est_02_marcoacademico']['general']['general'];
+}elseif(isset($Usu['acc']['general']['general']['general'])){
+	$Acc=$Usu['acc']['general']['general']['general'];
+}
+
+if($Acc<$minacc){
+    $Log['mg'][]=utf8_encode('no cuenta con permisos para modificar la planificación de este marco académico. \n minimo requerido: '.$minacc.' \ nivel disponible: '.$Acc);
+    $Log['tx'][]=print_r($Usu,true);
+    $Log['res']='err';
+    terminar($Log);
+}
+$Log['data']['acc']=$Acc;
+
+$idUsuario = $_SESSION[$CU]["usuario"]['id'];
+
+
+if(!isset($_POST['idpanel'])){
+	$_POST['idpanel']='';//no se envió solicitud de un panel específico (a un usuario con permisos se le enviarán todos los paneles, a un usuariosin permisos se le enviará uno solo
+}
+
+
+$variables_obligatorias=array(
+	'codMarco'=>'',
+	'nombre'=>'',
+	'descripcion'=>'',
+	'visible'=>'',
+	'mba'=>'',
+	'x'=>'',
+	'y'=>'',
+	'z'=>'',
+	'muestra_marco'=>''
+);
+
+foreach($variables_obligatorias as $k => $v){
+	if(!isset($_POST[$k])){
+		$Log['tx'][]='falta enviar variable '.$k;
+		$Log['res']='err';
+		terminar($Log);		 
+	}
+}
+
+if($_POST['x']===''){$linea_x= "x = null";}else{$linea_x="x = '".$_POST['x']."'";}
+if($_POST['y']===''){$linea_y= "y = null";}else{$linea_y="y = '".$_POST['y']."'";}
+if($_POST['z']===''){$linea_z= "z = null";}else{$linea_z="z = '".$_POST['z']."'";}
+
+
+$query="UPDATE
+			$DB.ref_comu_paneles
+		SET
+			titulo = '".$_POST['nombre']."',
+			descripcion = '".$_POST['descripcion']."',
+			visible = '".$_POST['visible']."',
+			mba = '".$_POST['mba']."',
+			$linea_x, 
+			$linea_y, 
+			$linea_z, 
+			muestra_marco = '".$_POST['muestra_marco']."'
+		WHERE
+		 id = '".$_POST['idp']."'
+		 AND
+		 ic_p_est_02_marcoacademico = '".$_POST['codMarco']."'
+";
+
+$Consulta = pg_query($ConecSIG, $query);
+if(pg_errormessage($ConecSIG)!=''){
+    $Log['tx'][]='error: '.pg_errormessage($ConecSIG);
+    $Log['tx'][]='query: '.$query;
+    $Log['mg'][]='error interno';
+    $Log['res']='err';
+    terminar($Log);	
+}
+
+$Log['res']="exito";
+terminar($Log);
